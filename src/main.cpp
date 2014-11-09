523008a95a93ee11712440c2824faddcf2df0ae5
ruic3429 <ruic3429@gmail.com>
2014-11-09 11:25:19 -0500

    Nov 8

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  example
+//
+//  Created by Alexander Clarke on 2014-11-07.
+//  Copyright (c) 2014 Alexander Clarke. All rights reserved.
+//
+
+#include <iostream>
+
+int main() {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
